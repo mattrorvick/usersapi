77b5ac8c7ce26a51bb3db6a44954ea185e0b2625
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-11-17 07:51:42 -0600

    user controller to handle request for users

--- src/main/java/com/tts/usersapi/controller/UserController.java
+++ src/main/java/com/tts/usersapi/controller/UserController.java
@@ -3,10 +3,15 @@ package com.tts.usersapi.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import com.tts.usersapi.model.User;
 import com.tts.usersapi.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +27,7 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
     
-    @GetMapping("/")
+    @GetMapping("/users")
     public List<User> getUsers(@RequestParam(value="state", required = false) String state) {
         
         if(state != null) {
@@ -34,36 +39,58 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public User getUser(@PathVariable(value="id") Long id) {
+    public ResponseEntity<Optional<User>> getUser(@PathVariable(value="id") Long id) {
         
        
 
         Optional<User> user = userRepository.findById(id);
         
-        if(user.isPresent()) {
-            return user.get();
-        } else {
-            User userError = new User();
-            userError.setFirstName("Error");
-            userError.setLastName("Error");
-            userError.setState("Error");
-            userError.setId((long) 1000000);
-            return userError;
+        if(!user.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        return new ResponseEntity<>(user, HttpStatus.OK);
+
+        // if(user.isPresent()) {
+        //     return user.get();
+        // } else {
+        //     User userError = new User();
+        //     userError.setFirstName("Error");
+        //     userError.setLastName("Error");
+        //     userError.setState("Error");
+        //     userError.setId((long) 1000000);
+        //     return userError;
+        // }
 
     }
 
-    @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    @PostMapping("/users")                                                  //BindingResult is class that accepts the error
+    public ResponseEntity<Void> createUser(@RequestBody @Valid User user, BindingResult bindingResult) {
+        System.out.println("Binding Result -- : " + bindingResult);
 
-        System.out.println(user);
+        if(bindingResult.hasErrors()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);       
+        }
+
         userRepository.save(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/users/{id}")
-    public void editUser(@PathVariable(value="id") Long id, @RequestBody User user) {
+    public ResponseEntity<Void> editUser(@PathVariable(value="id") Long id, @RequestBody @Valid User user, BindingResult bindingResult) {
+        
+        Optional<User> requestedUser = userRepository.findById(id);
+
+        if(!requestedUser.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        if(bindingResult.hasErrors()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);       
+        }
 
         userRepository.save(user);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/users/{id}")
